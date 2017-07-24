f20c98ac76caf5ede721b6e41699e6c56e3b0c1d
Robert Cooper <robert.cooper.qu@gmail.com>
2017-07-23 21:55:20 -0400

    Initial Rescue Time API integration

--- js/main.js
+++ js/main.js
@@ -73,3 +73,32 @@ $(document).ready(function() {
 
 // niceScroll || scrollbars resize
 $("body").getNiceScroll().resize();
+
+$.get('https://www.rescuetime.com/anapi/data?key=B63Yw5IF3RFY5pSxa4fnMnQS5adF_DFK4GWzPUOb&format=json&restrict_kind=overview', function(data) {
+
+		for (i = 0; i < data["rows"].length; i++) {
+			if (data["rows"][i][3] == "Software Development") {
+				var seconds = data["rows"][i][1];
+				var minutes = seconds/60;
+				var hours = minutes/60;
+				hours = hours.toFixed(2);
+				console.log(hours);
+				$(".web-development-hours").html("I've worked <span class='hours'>" + hours + "</span> hours on web development today.");
+			};
+		};
+});
+
+$.get('https://www.rescuetime.com/anapi/data?key=B63Yw5IF3RFY5pSxa4fnMnQS5adF_DFK4GWzPUOb&format=json&restrict_kind=productivity', function(data) {
+	var total_hours_distracted = 0;
+	for (i = 0; i < data["rows"].length; i++) {
+		if (data["rows"][i][3] == -1 | data["rows"][i][3] == -2 ) {
+			var seconds = data["rows"][i][1];
+			var minutes = seconds/60;
+			var hours = minutes/60;
+			total_hours_distracted += hours;
+		};
+	};
+	total_hours_distracted = total_hours_distracted.toFixed(2);
+	console.log(total_hours_distracted);
+	$(".distracted-hours").html("I've spent <span class='hours'>" + total_hours_distracted + "</span> hours on social media and other non productive things.");
+});

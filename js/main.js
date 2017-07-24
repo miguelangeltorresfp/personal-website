402369560664e2c301a27430f188c8f0d8b55dbf
Robert Cooper <robert.cooper.qu@gmail.com>
2017-07-24 00:21:51 -0400

    Integrated Strava API

--- js/main.js
+++ js/main.js
@@ -75,17 +75,17 @@ $(document).ready(function() {
 $("body").getNiceScroll().resize();
 
 $.get('https://www.rescuetime.com/anapi/data?key=B63Yw5IF3RFY5pSxa4fnMnQS5adF_DFK4GWzPUOb&format=json&restrict_kind=overview', function(data) {
-
+		var hours = 0;
 		for (i = 0; i < data["rows"].length; i++) {
 			if (data["rows"][i][3] == "Software Development") {
 				var seconds = data["rows"][i][1];
 				var minutes = seconds/60;
 				var hours = minutes/60;
-				hours = hours.toFixed(2);
-				console.log(hours);
-				$(".web-development-hours").html("I've worked <span class='hours'>" + hours + "</span> hours on web development today.");
+				break;
 			};
 		};
+		hours = hours.toFixed(2);
+		$(".web-development-hours").html("<span class='api-data'>" + hours + "</span> hours on web development.");
 });
 
 $.get('https://www.rescuetime.com/anapi/data?key=B63Yw5IF3RFY5pSxa4fnMnQS5adF_DFK4GWzPUOb&format=json&restrict_kind=productivity', function(data) {
@@ -100,5 +100,21 @@ $.get('https://www.rescuetime.com/anapi/data?key=B63Yw5IF3RFY5pSxa4fnMnQS5adF_DF
 	};
 	total_hours_distracted = total_hours_distracted.toFixed(2);
 	console.log(total_hours_distracted);
-	$(".distracted-hours").html("I've spent <span class='hours'>" + total_hours_distracted + "</span> hours on social media and other non productive things.");
+	$(".distracted-hours").html("<span class='api-data'>" + total_hours_distracted + "</span> hours on social media and other non productive things.");
+});
+
+$.get('https://www.strava.com/api/v3/athlete/activities?access_token=6f1ce73011107949166d10ea05e522443eab24c2', function(data) {
+	for (i=0; i < data.length; i++) {
+		if (data[i]["type"] == "Run") {
+			var duration = new Date(null);
+			duration.setSeconds(data[i]["moving_time"]);
+			var converted_duration = duration.toISOString().substr(11, 8);
+			var distance_meters = data[i]["distance"];
+			var distance_km = distance_meters/1000;
+			distance_km = distance_km.toFixed(2);
+			$(".running-distance").html("<span class='api-data'>" + distance_km + "</span>kms ran.");
+			$(".running-duration").html("<span class='api-data'>" + converted_duration + "</span> in duration.");
+			break;
+		}
+	}
 });

ef251a587557c072de6465329337ed40c0f9050b
Robert Cooper <robert.cooper.qu@gmail.com>
2017-07-24 20:43:41 -0400

    Uncommented the Rescue Time API JavaScript and HTML

--- distribution/js/main.js
+++ distribution/js/main.js
@@ -76,33 +76,33 @@ $("body").getNiceScroll().resize();
 
 // API Calls
 
-// $.get('https://www.rescuetime.com/anapi/data?key=B63Yw5IF3RFY5pSxa4fnMnQS5adF_DFK4GWzPUOb&format=json&restrict_kind=overview', function(data) {
-// 		var hours = 0;
-// 		for (i = 0; i < data["rows"].length; i++) {
-// 			if (data["rows"][i][3] == "Software Development") {
-// 				var seconds = data["rows"][i][1];
-// 				var minutes = seconds/60;
-// 				var hours = minutes/60;
-// 				break;
-// 			};
-// 		};
-// 		hours = hours.toFixed(2);
-// 		$(".web-development-hours").html("<span class='api-data'>" + hours + "</span> hours on web development.");
-// });
+$.get('https://www.rescuetime.com/anapi/data?key=B63Yw5IF3RFY5pSxa4fnMnQS5adF_DFK4GWzPUOb&format=json&restrict_kind=overview', function(data) {
+		var hours = 0;
+		for (i = 0; i < data["rows"].length; i++) {
+			if (data["rows"][i][3] == "Software Development") {
+				var seconds = data["rows"][i][1];
+				var minutes = seconds/60;
+				var hours = minutes/60;
+				break;
+			};
+		};
+		hours = hours.toFixed(2);
+		$(".web-development-hours").html("<span class='api-data'>" + hours + "</span> hours on web development.");
+});
 
-// $.get('https://www.rescuetime.com/anapi/data?key=B63Yw5IF3RFY5pSxa4fnMnQS5adF_DFK4GWzPUOb&format=json&restrict_kind=productivity', function(data) {
-// 	var total_hours_distracted = 0;
-// 	for (i = 0; i < data["rows"].length; i++) {
-// 		if (data["rows"][i][3] == -1 | data["rows"][i][3] == -2 ) {
-// 			var seconds = data["rows"][i][1];
-// 			var minutes = seconds/60;
-// 			var hours = minutes/60;
-// 			total_hours_distracted += hours;
-// 		};
-// 	};
-// 	total_hours_distracted = total_hours_distracted.toFixed(2);
-// 	$(".distracted-hours").html("<span class='api-data'>" + total_hours_distracted + "</span> hours on social media and other non productive things.");
-// });
+$.get('https://www.rescuetime.com/anapi/data?key=B63Yw5IF3RFY5pSxa4fnMnQS5adF_DFK4GWzPUOb&format=json&restrict_kind=productivity', function(data) {
+	var total_hours_distracted = 0;
+	for (i = 0; i < data["rows"].length; i++) {
+		if (data["rows"][i][3] == -1 | data["rows"][i][3] == -2 ) {
+			var seconds = data["rows"][i][1];
+			var minutes = seconds/60;
+			var hours = minutes/60;
+			total_hours_distracted += hours;
+		};
+	};
+	total_hours_distracted = total_hours_distracted.toFixed(2);
+	$(".distracted-hours").html("<span class='api-data'>" + total_hours_distracted + "</span> hours on social media and other non productive things.");
+});
 
 $.get('https://www.strava.com/api/v3/athlete/activities?access_token=6f1ce73011107949166d10ea05e522443eab24c2', function(data) {
 	for (i=0; i < data.length; i++) {

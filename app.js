61c95078ed449b31f766d7cb2f86af589ea54c7a
Robert Cooper <robert.cooper.qu@gmail.com>
2017-09-03 12:37:21 -0400

    Redirect to https for non www URLs

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ router.all(/.*/, function(req, res, next) {
   if (host.match(/^www\..*/i)) {
     next();
   } else {
-    res.redirect(301, "http://www." + host);
+    res.redirect(301, "https://www." + host);
   }
 });
 

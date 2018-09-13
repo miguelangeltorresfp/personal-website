2dbe549c03a5158f6c3ca22145837159cf42d194
Robert Cooper <robert.cooper.qu@gmail.com>
2018-09-12 23:50:25 -0400

    Ran prettier on app.js and gulpfile.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const proxy = require('express-http-proxy');
+const proxy = require("express-http-proxy");
 const express = require("express");
 const app = express();
 const router = express.Router();
 
 // Allow environment variables to be access through process.env
-require('dotenv').config();
+require("dotenv").config();
+
+// Proxy requests to the local lambda functions server
+app.use("/.netlify/functions", proxy("http://localhost:9000"));
 
 // Map URLs to folders
 app.use("/documents", express.static(`app/documents`));
@@ -14,9 +17,6 @@ app.use("/img", express.static(`app/img`));
 app.use("/fonts", express.static(`app/fonts`));
 app.use("/public", express.static(`app`));
 
-// Proxy requests to the local lambda functions server
-app.use("/.netlify/functions", proxy("http://localhost:9000"));
-
 app.set("views");
 app.set("view engine", "pug");
 
@@ -24,33 +24,33 @@ app.use(router);
 
 // Make sure all URLs use www.
 router.all(/.*/, function(request, response, next) {
-    const host = request.get("host");
-    if (host === "robertcooper.me") {
-        if (host.match(/^www\..*/i)) {
-            next()
-        } else {
-            response.redirect(301, "https://www." + host)
-        }
+  const host = request.get("host");
+  if (host === "robertcooper.me") {
+    if (host.match(/^www\..*/i)) {
+      next();
+    } else {
+      response.redirect(301, "https://www." + host);
     }
-    next()
+  }
+  next();
 });
 
 router.get("/", (request, response) => {
-    response.render("index", { environment: process.env.NODE_ENV })
+  response.render("index", { environment: process.env.NODE_ENV });
 });
 
 app.use((request, response, next) => {
-    const err = new Error("Not Found");
-    err.status = 404;
-    next(err);
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
 app.use((error, request, response) => {
-    response.status(error.status);
-    console.error(error.stack);
-    response.send(error.stack);
+  response.status(error.status);
+  console.error(error.stack);
+  response.send(error.stack);
 });
 
 app.listen(8080, () => {
-    console.log("The application is running on localhost:8080!");
+  console.log("The application is running at http://localhost:8080!");
 });
